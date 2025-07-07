362a68493db99b7cc4fad4ba960756d8c17f9db7
Gardner Sullivan <gardnersullivan9@gmail.com>
2025-07-06 18:28:46 -0600

    Refactor Player class by removing Enemy class definition

--- characters.py
+++ characters.py
@@ -12,8 +12,3 @@ class Character:
 class Player(Character):
     # Use default_factory so each player gets a new list
     inventory: List[str] = field(default_factory=list)
-
-@dataclass
-class Enemy(Character):
-    # Items the player can collect after defeating this foe
-    loot: List[str] = field(default_factory=list)
